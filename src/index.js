5d1cda7743ddc0e7b47154aa12841ce1f5e7f490
LesokoM <shokijhb@gmail.com>
2025-05-29 22:36:26 +0200

    completed AI poem
    everything working now

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
-function generatestory(event){
+  async function generatestory(event){
     event.preventDefault();
-    console.log("we are generating a story");
+    let page = document.querySelector(".writingpage")
+    page.innerHTML = "Generating a story....";
 
     let userPrompt = document.getElementById("prompttext").value
     console.log(userPrompt)
 
-    let context  = "You are from tht 1800s. Telling old shakespearean stories"
+    let context  = "You are from tht 1800s. Telling old shakespearean short poems"
     let apiKey = "48bf6of5134a63ab203acfc50316tbd4"
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${userPrompt}&context=${context}&key=${apiKey}`
 
-    let response  = axios.get(apiUrl)
-
-
+    let response  = await axios.get(apiUrl)
 
     console.log(response.data.answer)
+
+    new Typewriter('.writingpage', {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 5,
+    
+    });
+
+
+
+   
 
 
 }
 
 
 let apiKey = "48bf6of5134a63ab203acfc50316tbd4"
-let context = "You are from the 1800s. You tell old viking stories in a semi shakespearean language"
+let context = "Each story must be maximum 200 words. You are from the 1800s. You tell old viking stories in a semi shakespearean language. "
 
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
